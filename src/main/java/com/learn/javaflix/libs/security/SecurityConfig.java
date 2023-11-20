0aa790550ee5d27a40d6fe3663a759acf25e21df
rocha19 <rochaaqueologia@outlook.com>
2023-11-20 13:43:04 -0300

    feat: definitive cors config

--- src/main/java/com/learn/javaflix/libs/security/SecurityConfig.java
+++ src/main/java/com/learn/javaflix/libs/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.learn.javaflix.libs.security;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
@@ -22,6 +26,17 @@ public class SecurityConfig {
       auth.requestMatchers("/api/login").permitAll();
       auth.anyRequest().authenticated();
     }).addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
+    http
+            .cors(httpSecurityCorsConfigurer -> {
+                CorsConfiguration configuration = new CorsConfiguration();
+                configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080", "http://localhost:5173"));
+                configuration.setAllowedMethods(Arrays.asList("*"));
+                configuration.setAllowedHeaders(Arrays.asList("*"));
+                configuration.setAllowCredentials(true);
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+                httpSecurityCorsConfigurer.configurationSource(source);
+            });
     return http.build();
   }
 
